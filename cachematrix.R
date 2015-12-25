e82e1158afa5b245679180c001729ca46092b5d1
nnair12 <nikhilnairca@gmail.com>
2015-12-24 16:40:31 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates and returns a list of functions that can be
+## used to set an inverted matrix in the cache or to get an inverted
+## matrix from the cache.
+makeCacheMatrix <- function(x = matrix()) 
+{
+    cacheMatrix <- NULL # for cached value, initialized as null
+    
+    ## to create a matrix in the current environment
+    set <- function (y)
+    {
+      x <<- y
+      cacheMatrix <<- NULL
+    }
+    
+    ## to obtain the original matrix
+    get <- function() {
+      x
+    }
+    
+    ## Inverts the matrix and stores it in the cache
+    setInverse <- function (solvedMatrix) {
+      cacheMatrix <<- solvedMatrix
+    }
+    
+    ## Obtains the inverted matrix from the cache
+    getInverse <- function() {
+      cacheMatrix
+    }
+    
+    ## Returns the list of created functions
+    list (set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function computers the inverse of the special matrix returned
+## by makeCacheMatrix. If the inverse has already been calculated
+## (and the matrix has not changed), then cachesolve should retrieve
+## the inverse from the cache.
+cacheSolve <- function(x, ...) 
+{
+    inverse <- x$getInverse()
+    
+    ## Runs if the inverse has already been calculated
+    ## (and the matrix hasn't been changed).
+    if (!is.null(inverse)) 
+    {
+        message ("Getting cached data")
+        return (inverse)
+    }
+    
+    ## If inverse wasn't calculated or matrix was changed
+    newMatrix <- x$get()
+    inverse <- solve (newMatrix, ...)
+    x$setInverse(inverse)
+    inverse
 }
